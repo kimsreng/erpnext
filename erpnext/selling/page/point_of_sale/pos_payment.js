4a443b4bb75a7b4da22c771d374986c83119ab5b
kimsreng <kimsreng1@gmail.com>
2021-08-09 20:01:06 +0700

    Update pos_payment.js

--- erpnext/selling/page/point_of_sale/pos_payment.js
+++ erpnext/selling/page/point_of_sale/pos_payment.js
@@ -42,7 +42,7 @@ erpnext.PointOfSale.Payment = class {
 	}
 
 	make_invoice_fields_control() {
-		frappe.db.get_doc("POS Settings", undefined).then((doc) => {
+		frappe.db.get_company_doc("POS Settings").then((doc) => {
 			const fields = doc.invoice_fields;
 			if (!fields.length) return;
 
