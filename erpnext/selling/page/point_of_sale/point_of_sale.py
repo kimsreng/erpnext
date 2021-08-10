4b4ef5d9776fd292f5a4415000b7f5f42f8d466e
kimsreng <kimsreng1@gmail.com>
2021-08-10 13:52:12 +0700

    Update point_of_sale.py

--- erpnext/selling/page/point_of_sale/point_of_sale.py
+++ erpnext/selling/page/point_of_sale/point_of_sale.py
@@ -237,18 +237,18 @@ def get_past_order_list(search_term, status, limit=20):
 	invoice_list = []
 
 	if search_term and status:
-		invoices_by_customer = frappe.db.get_all('POS Invoice', filters={
+		invoices_by_customer = frappe.db.get_list('POS Invoice', filters={
 			'customer': ['like', '%{}%'.format(search_term)],
 			'status': status
 		}, fields=fields)
-		invoices_by_name = frappe.db.get_all('POS Invoice', filters={
+		invoices_by_name = frappe.db.get_list('POS Invoice', filters={
 			'name': ['like', '%{}%'.format(search_term)],
 			'status': status
 		}, fields=fields)
 
 		invoice_list = invoices_by_customer + invoices_by_name
 	elif status:
-		invoice_list = frappe.db.get_all('POS Invoice', filters={
+		invoice_list = frappe.db.get_list('POS Invoice', filters={
 			'status': status
 		}, fields=fields)
 
